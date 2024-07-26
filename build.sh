ee8d3b371275c8764c577ab5039050532da523b0
nandisha.d@think41.com <nandisha.d@think41.com>
2024-07-26 12:42:31 +0530

    trying to build at cloud 0.0.3

--- build.sh
+++ build.sh
@@ -9,5 +9,8 @@ npm install
 # Run the build command
 npm run build
 
+# Move the dist folder to the main directory
+mv dist ../
+
 # Optionally, you can navigate back to the root directory if needed
 cd ..
